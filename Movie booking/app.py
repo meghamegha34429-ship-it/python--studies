4295f7115306cf1ab33cdbe9fa5b6f60cb8b2c5a
meghamegha34429-ship-it <meghamegha34429@gmail.com>
2026-06-21 11:30:08 +0530

    app.py

--- Movie booking/app.py
+++ Movie booking/app.py
@@ -1,23 +1,63 @@
 from flask import Flask, render_template, request
-from movie import MovieBooking
 
 app = Flask(__name__)
 
-booking = MovieBooking()
-
+# Home Page
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/book")
-def book():
 
-    name = request.args.get("name")
-    movie = request.args.get("movie")
-    tickets = int(request.args.get("tickets"))
+# Check Booking Details
+@app.route("/search", methods=["POST"])
+def search():
 
-    data = booking.book_ticket(name, movie, tickets)
+    name = request.form["name"]
+    movie = request.form["movie"]
+    tickets = int(request.form["tickets"])
 
-    return render_template("result.html", data=data)
+    price = 150
 
-app.run(debug=True)
+    if movie.lower() == "leo":
+        show_time = "10:00 AM"
+    elif movie.lower() == "goat":
+        show_time = "2:00 PM"
+    elif movie.lower() == "vikram":
+        show_time = "6:00 PM"
+    else:
+        show_time = "9:00 PM"
+
+    total = tickets * price
+
+    return render_template(
+        "booking.html",
+        name=name,
+        movie=movie,
+        tickets=tickets,
+        show_time=show_time,
+        total=total
+    )
+
+
+# Confirm Booking
+@app.route("/confirm", methods=["POST"])
+def confirm():
+
+    name = request.form["name"]
+    movie = request.form["movie"]
+    tickets = request.form["tickets"]
+    show_time = request.form["show_time"]
+    total = request.form["total"]
+
+    return render_template(
+        "success.html",
+        name=name,
+        movie=movie,
+        tickets=tickets,
+        show_time=show_time,
+        total=total
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
